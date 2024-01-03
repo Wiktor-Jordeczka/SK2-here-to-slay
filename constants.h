65f2a61e2bfec21ebd83d6b8a8700f475aaa5634
Wiktor-Jordeczka <135605897+Wiktor-Jordeczka@users.noreply.github.com>
2024-01-03 14:30:40 +0100

    Wstępna implementacja epoll

--- constants.h
+++ constants.h
@@ -1,6 +1,7 @@
-int PORT = 44444; // port serwera
-int opt = 1; // dla setsockopt()
-int maxNumOfPlayers = 4; // maksymalna liczba graczy w jednej grze
-int maxNumOfGames = 4; // maksymalna liczba gier
-int maxNumOfConnections = maxNumOfPlayers * maxNumOfGames + 1; // maksymalna liczba połączeń
-int bufferSize = 512; // rozmiar bufora
+const int PORT = 44444; // port serwera
+const int opt = 1; // dla setsockopt()
+const int maxNumOfPlayers = 4; // maksymalna liczba graczy w jednej grze
+const int maxNumOfGames = 4; // maksymalna liczba gier
+const int maxNumOfConnections = maxNumOfPlayers * maxNumOfGames + 1; // maksymalna liczba połączeń
+const int bufferSize = 4096; // rozmiar bufora
+const int maxEpollEvents = maxNumOfConnections; // maksymalna liczba wydarzeń epoll
--- server_TCP_epoll.cpp
+++ server_TCP_epoll.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <sys/socket.h>
-#include <poll.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
 #include "constants.h"
 
 using namespace std;
 
+char buf[bufferSize]; // bufor ogólnego przeznaczenia
+
+void testClient(int clientFD){
+    char clientBuf[bufferSize]; // bufor klienta
+    memset(&clientBuf, '\0', bufferSize);
+    recv(clientFD, &clientBuf, bufferSize, 0); // opcje... potestować
+    cout << clientBuf<<endl;
+}
+
 int main(int argc, char **argv){
     cout<<"Działamy na porcie "<<PORT<<endl;
+    cerr<<"test"<<endl;
 
-    char buf[bufferSize]; // bufor
-
-    // Tworzymy strukturę adresową
+    // Tworzymy strukturę adresową serwera
     sockaddr_in localAddress{
         .sin_family = AF_INET,
         .sin_port = htons(PORT),
@@ -19,14 +29,77 @@ int main(int argc, char **argv){
     };
 
     // Tworzymy gniazdo
-    int socketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    setsockopt(socketFD, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    int serverFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    setsockopt(serverFD, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
     // Łączymy gniazdo z adresem
-    bind(socketFD, (sockaddr*) &localAddress, sizeof(localAddress));
+    if(bind(serverFD, (sockaddr*) &localAddress, sizeof(localAddress)) != 0){
+        cerr << "Bind failed"<<endl;
+        close(serverFD);
+        return -1;
+    }
 
     // Ustawiamy nasłuchiwanie
-    listen(socketFD, maxNumOfConnections);
+    if(listen(serverFD, maxNumOfConnections) == -1){
+        cerr << "Listen failed"<<endl;
+        close(serverFD);
+        return -1;
+    }
 
     // Przygotowujemy mechanizm epoll
+    int epollFD = epoll_create1(0);
+    if (epollFD == -1){
+        cerr << "epoll create failed"<<endl;
+        close(serverFD);
+        return -1;
+    }
+
+    // Dodajemy socket do epoll
+    epoll_event event; // struktura do definiowania pojedynczego zdarzenia
+
+    event.events = EPOLLIN;
+    event.data.fd = serverFD;
+    if(epoll_ctl(epollFD, EPOLL_CTL_ADD, serverFD, &event) == -1){
+        cerr << "epoll socket add failed"<<endl;
+        close(serverFD);
+        close(epollFD);
+        return -1;
+    }
+
+    cout << "Server started" << endl;
+
+    epoll_event events[maxEpollEvents]; // Tablica zdarzeń epoll
+
+    while(true){
+        int numOfEvents = epoll_wait(epollFD, events, maxEpollEvents, -1); // -1 infinite, 0 instant
+
+        for(int i=0; i<numOfEvents; i++){
+            // Nowe połączenie
+            if(events[i].data.fd == serverFD){
+                sockaddr_in clientAddress; // adres klienta
+                socklen_t cal = sizeof(clientAddress);
+                int clientFD = accept(serverFD, (sockaddr*) &clientAddress, &cal);
+                if(clientFD == -1){
+                    cerr << "connection failed"<<endl;
+                    continue;
+                }
+
+                // Dodajemy klienta do epoll
+                event.events = EPOLLIN;
+                event.data.fd = clientFD;
+                if(epoll_ctl(epollFD, EPOLL_CTL_ADD, clientFD, &event) == -1){
+                    cerr << "epoll client add failed"<<endl;
+                    close(clientFD);
+                    continue;
+                }
+            } else {
+                // Obsługa klienta
+                testClient(events[i].data.fd);
+            }
+        }
+    }
+
+    close(serverFD);
+    close(epollFD);
+    return 0;
 }
